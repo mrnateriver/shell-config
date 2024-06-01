307e7b92c9d1d4c6c3d376c15cf32a05dca82cd5
Evgenii Dobrovidov <mr.nate.river@gmail.com>
2024-06-01 14:12:59 +0200

    fix: sed on Mac OS in setup.sh

--- setup.sh
+++ setup.sh
@@ -11,7 +11,8 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 # Python OpenAI API CLI client
 pipx install openai
 
-sed -i 's/^\W*plugins=\(.*\)$/plugins=(git azure kubectl kubectx docker docker-compose yarn npm)/g' .zshrc
+# The first empty arg is required on Mac OS/BSD -- remove it for Linux if it causes issues
+sed -i '' 's/^\W*plugins=\(.*\)$/plugins=(git azure kubectl kubectx docker docker-compose yarn npm)/g' .zshrc
 
 echo "\n[[ ! -f ~/.zshrc.custom ]] || source ~/.zshrc.custom" >> ~/.zshrc
 
